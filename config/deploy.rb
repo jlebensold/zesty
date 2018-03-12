d1483840b4cc3e4518533f5866c08a9f2828227e
Jon Lebensold <jon@lebensold.ca>
2018-03-12 16:03:15 -0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,7 @@ set :forward_agent, true # SSH forward_agent.
 # shared dirs and files will be symlinked into the app-folder by the
 # 'deploy:link_shared_paths' step.
 set :shared_dirs, fetch(:shared_dirs, []).push("assets")
+set :term_mode, nil
 # set :shared_files, fetch(:shared_files, []).push(config/database.yml', 'config/secrets.yml')
 
 # This task is the environment that is loaded for all remote run commands, such as
