5a3bcd8b7c35bf17f3f7f03bae12ace4f38b2591
Jon Lebensold <jon@lebensold.ca>
2018-03-14 10:14:10 -0400

    adding fog

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,3 +5,11 @@ Paperclip.options[:content_type_mappings] = {
   pb: ["application/octet-stream"],
   log: "text/plain"
 }
+
+
+
+if Rails.env.production?
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+  Paperclip::Attachment.default_options[:url] = ':gcs_domain_url'
+  Paperclip::Attachment.default_options[:fog_directory] = ENV['GCS_BUCKET_NAME']
+end
