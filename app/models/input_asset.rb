12c22970ff8674bf72ebdf177850b68ad478d793
Jon Lebensold <jon@lebensold.ca>
2018-03-14 23:24:16 -0400

    google cloud

--- app/models/input_asset.rb
+++ app/models/input_asset.rb
@@ -13,7 +13,7 @@ class InputAsset < ApplicationRecord
         credentials: ENV["GCS_KEYFILE"]
       )
       bucket = storage.bucket ENV["GCS_BUCKET_NAME"]
-      bucket.file(attachment.path(:original))
+      file = bucket.file(attachment.path(:original))
       file.download copy_path
     else
       attachment.copy_to_local_file(:original, copy_path)
